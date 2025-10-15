2a46c77bdb4bb20cd63d34307421ac7bbef1968a
Jeremy Scheff <jdscheff@gmail.com>
2025-10-15 11:49:09 -0400

    Failing multi tests

---
+++ src/test/multi.ts
@@ -0,0 +1,24 @@
+import { assert, test } from "vitest";
+import { JSONParseStream } from "../JSONParseStream.ts";
+import { makeReadableStreamFromJson } from "./utils.ts";
+
+const separators = ["", "\n", "\r\n", " ", " \n ", "␞"];
+for (const separator of separators) {
+	test.skip(`Multiple JSON objects with ${JSON.stringify(separator)} in between`, async () => {
+		const objects = [{ a: 1 }, { a: 2 }, { foo: "bar" }, { a: 3 }];
+		const json = objects
+			.map((object) => JSON.stringify(object))
+			.join(separator);
+		const stream = makeReadableStreamFromJson(json).pipeThrough(
+			new JSONParseStream(["$.a"], {
+				multi: true,
+			}),
+		);
+		const chunks = await Array.fromAsync(stream);
+		assert.deepStrictEqual(chunks, [
+			{ value: 1, index: 0 },
+			{ value: 2, index: 0 },
+			{ value: 3, index: 0 },
+		]);
+	});
+}
